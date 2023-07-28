e33514e9b9108a34da001ab3091e5a9276630fc5
ss749236 <ss749236@gmail.com>
2023-07-28 18:05:33 +0530

    insta

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './App.scss';
 import SideBar from './components/side-bar';
 import MainPanel from './components/main-panel';
 
+
 function App() {
   return (
     <div className="App-container">
       <SideBar />
       <MainPanel />
+
     </div>
   );
 }
--- src/components/main-panel.tsx
+++ src/components/main-panel.tsx
@@ -1,4 +1,4 @@
-import { postInfo } from "../data/post-data";
+import { postInfo } from "../data/posts-data";
 import { storyData } from "../data/story-data";
 import { storiesData } from "../interface/stories-data";
 import "./main-panel.scss"
@@ -8,7 +8,8 @@ import { AiOutlineHeart } from "react-icons/ai"
 import { TbMessageCircle } from "react-icons/tb"
 import { VscBookmark } from "react-icons/vsc"
 import { HiOutlineDotsHorizontal } from "react-icons/hi"
-
+import "./side-panel.scss"
+import { BiCopyright } from "react-icons/bi"
 
 
 const ProfileIcon = (props: storiesData) => {
@@ -18,13 +19,13 @@ const ProfileIcon = (props: storiesData) => {
             <img src={props.icon} id="img" alt="" />
             {props.name}
         </div>
-    )
+    );
 }
 
 const Post = (props: postData) => {
     return (
         <div id="post">
-            <div id="post-Heading"><img id="image" src={props.postIcon} alt="" />{props.postHeading}<HiOutlineDotsHorizontal id="line-dot" /></div>
+            <div id="post-Heading"><span><img id="image" src={props.postIcon} alt="" /></span>  <span id="headding">{props.postHeading}</span><span id="line-dot"><HiOutlineDotsHorizontal /></span></div>
             <div id="post-pic"><img id="image2" src={props.post} alt="" /></div>
             <div id="icons"><AiOutlineHeart id="heart" /><TbMessageCircle id="mssg" /><VscBookmark id="book-mark" /></div>
             <div id="likes">{props.likes}</div>
@@ -44,6 +45,16 @@ const MainPanel = () => {
                     {storyData.map((storyData) => (<ProfileIcon name={storyData.name} icon={storyData.icon} />))}
                 </div>
                 <div id="about-posts">{postInfo.map((postInfo) => (<Post postIcon={postInfo.postIcon} postHeading={postInfo.postHeading} post={postInfo.post} likes={postInfo.likes} caption={postInfo.caption} more={postInfo.more} view={postInfo.view} addComment={postInfo.addComment} />))}</div>
+            </div>
+            <div id="side-panel">
+                <div id="personal">
+                    <div id="profile-photo"><img id="pic" src="https://tse2.explicit.bing.net/th?id=OIP.sQjNfZ7e2kRhqwJ_Xj-_JAHaHa&pid=Api&P=0&h=180" alt="" /></div>
+                    <div id="name">Shakira_34769<div id="original">
+                        shakira wembolt</div></div>
+                    <div id="switch">Switch</div>
+                </div>
+                <div id="others">About . Help . Press . API . Jobs . Privacy . Terms . Locations . Language . Meta Verified </div>
+                <div id="copyright"><BiCopyright id="copy" />2023 INSTAGRAM FORM META</div>
             </div>
 
         </div>
